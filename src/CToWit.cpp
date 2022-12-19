e392fa2d1bc8736da4434a1ae6df1d693121cd14
Yingchi Long <i@lyc.dev>
2022-12-19 17:33:26 +0800

    [ctowit] trunc llvm header

--- src/CToWit.cpp
+++ src/CToWit.cpp
@@ -1,16 +1,3 @@
-//===- PrintFunctionNames.cpp ---------------------------------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-//
-// Example clang plugin which simply prints the names of all the top-level decls
-// in the input file.
-//
-//===----------------------------------------------------------------------===//
-
 #include "clang/AST/AST.h"
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/RecursiveASTVisitor.h"
