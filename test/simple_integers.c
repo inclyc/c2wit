fbc464a344998b21cbc66e951a33e43343616821
Yingchi Long <i@lyc.dev>
2022-12-19 17:29:54 +0800

    [ctowit] implement basic integers codegen

---
+++ test/simple_integers.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+
+struct Test {
+  char c;
+  short s;
+  int i;
+  long l;
+  long long ll;
+  unsigned char uc;
+  unsigned short us;
+  unsigned int ui;
+  unsigned long ul;
+  unsigned long long ull;
+
+  uint32_t u32;
+  uint64_t u64;
+  int32_t i32;
+  int64_t i64;
+};
