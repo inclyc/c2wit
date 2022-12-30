8058727d8004a63a065a0023cc957735b0a8a838
Yingchi Long <i@lyc.dev>
2022-12-30 13:13:02 +0800

    [meson] migrate to meson build

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -9,14 +9,16 @@ config.suffixes = ['.c', '.cpp', '.i', '.cppm', '.m', '.mm', '.cu', '.hip', '.hl
 # testFormat: The test format to use to interpret tests.
 config.test_format = lit.formats.ShTest()
 
-workspace_root = os.path.dirname(__file__)
+test_root = os.path.dirname(__file__)
+workspace_root = os.path.join(test_root, "..")
+
 
 # test_source_root: The root path where tests are located.
-config.test_source_root = workspace_root
+config.test_source_root = test_root
 
 # test_exec_root: The root path where tests should be run.
-config.test_exec_root = os.path.join(workspace_root, 'test')
+config.test_exec_root = workspace_root
 
 
 config.substitutions.append(
-    ('%{ctowit_clang}', f'clang -fsyntax-only -fplugin={workspace_root}/build/CToWit.so -Xclang -plugin -Xclang ctowit'))
+    ('%{ctowit_clang}', f'clang -fsyntax-only -fplugin={workspace_root}/build/libCToWit.so -Xclang -plugin -Xclang ctowit'))
