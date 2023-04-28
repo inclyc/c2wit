6e9766e3a41b954036c8f98dd084c7c163919563
Yingchi Long <i@lyc.dev>
2023-04-28 14:58:46 +0800

    lib/Support: remove fmt::format and using llvm native "formatv"

--- lib/Support.cpp
+++ lib/Support.cpp
@@ -1,15 +1,17 @@
 #include "Support.h"
-#include <fmt/core.h>
+#include <llvm/Support/FormatVariadic.h>
 
 namespace clang::c2wit {
+
+using llvm::formatv;
 
 std::string getWitNameFromBuiltinType(const ASTContext &Cxt,
                                       const BuiltinType *BT) {
   if (BT->isInteger())
-    return fmt::format("{}{}", BT->isSignedInteger() ? "i" : "u",
-                       Cxt.getTypeSize(BT));
+    return formatv("{0}{1}", BT->isSignedInteger() ? "i" : "u",
+                   Cxt.getTypeSize(BT));
   if (BT->isFloatingType())
-    return fmt::format("f{}", Cxt.getTypeSize(BT));
+    return formatv("f{0}", Cxt.getTypeSize(BT));
   return "????";
 }
 
